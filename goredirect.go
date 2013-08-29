373ff6c42f0acd14088c0d1cdbc9aa27da78384c
Anton Lindström <lindztr@gmail.com>
2013-08-29 20:28:27 +0200

    fix formating per gofmt

--- goredirect.go
+++ goredirect.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "strings"
-    "time"
-    )
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
 
 const RedirectType = 301
 
@@ -17,57 +17,57 @@ var RedirectMap map[string]string
 
 // Server, make sure we fail as fast as possible
 func main() {
-    loadConfiguration()
+	loadConfiguration()
 
-    s := &http.Server {
-        Addr:           ":8080",
-        ReadTimeout:    1 * time.Second,
-        WriteTimeout:   1 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-    }
+	s := &http.Server{
+		Addr:           ":8080",
+		ReadTimeout:    1 * time.Second,
+		WriteTimeout:   1 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 
-    http.HandleFunc("/", HostRedirect)
-    http.HandleFunc("/reload", ReloadConfig)
+	http.HandleFunc("/", HostRedirect)
+	http.HandleFunc("/reload", ReloadConfig)
 
-    log.Printf("Serving requests on :8080")
-    log.Fatal(s.ListenAndServe())
+	log.Printf("Serving requests on :8080")
+	log.Fatal(s.ListenAndServe())
 }
 
 // Redirect to map
 func HostRedirect(w http.ResponseWriter, req *http.Request) {
-    requestUrl    := strings.Split(req.Host, ":")[0]
-    redirectUrl   := RedirectMap[requestUrl]
+	requestUrl := strings.Split(req.Host, ":")[0]
+	redirectUrl := RedirectMap[requestUrl]
 
-    if redirectUrl == "" {
-        http.Error(w, "503: Could not map request!", 503)
-        log.Printf("code: 503, request: %s, redirect: empty!, path: %s", requestUrl, req.URL.Path)
-        return
-    }
+	if redirectUrl == "" {
+		http.Error(w, "503: Could not map request!", 503)
+		log.Printf("code: 503, request: %s, redirect: empty!, path: %s", requestUrl, req.URL.Path)
+		return
+	}
 
-    log.Printf("code: %d, request: %s, redirect: %s, path: %s", RedirectType, requestUrl, redirectUrl, req.URL.Path)
-    http.Redirect(w, req, redirectUrl, RedirectType)
+	log.Printf("code: %d, request: %s, redirect: %s, path: %s", RedirectType, requestUrl, redirectUrl, req.URL.Path)
+	http.Redirect(w, req, redirectUrl, RedirectType)
 }
 
 // Handler to reload configuration
 func ReloadConfig(w http.ResponseWriter, req *http.Request) {
-    loadConfiguration()
-    log.Printf("code: 200, reload")
-    fmt.Fprintf(w, "OK, reloaded.")
+	loadConfiguration()
+	log.Printf("code: 200, reload")
+	fmt.Fprintf(w, "OK, reloaded.")
 }
 
 // Load configuration
 func loadConfiguration() {
-    bytes, err  := ioutil.ReadFile("./config.json")
+	bytes, err := ioutil.ReadFile("./config.json")
 
-    if err != nil {
-        log.Printf("%s\n", err)
-        os.Exit(1)
-    }
+	if err != nil {
+		log.Printf("%s\n", err)
+		os.Exit(1)
+	}
 
-    parseErr := json.Unmarshal(bytes, &RedirectMap)
+	parseErr := json.Unmarshal(bytes, &RedirectMap)
 
-    if parseErr != nil {
-        log.Fatal("%s\n", err)
-        os.Exit(1)
-    }
+	if parseErr != nil {
+		log.Fatal("%s\n", err)
+		os.Exit(1)
+	}
 }
